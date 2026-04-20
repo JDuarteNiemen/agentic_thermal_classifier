3e726edfd0fe393bd3c9c1942604bc219e0ccb33
Jaime Duarte-Niemen <jaimeduarteniemen@Jaimes-MacBook-Pro.local>
2026-04-20 14:37:36 +0100

    new repo

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TO DO LIST
+
+## Research
+research thermal ranges, psychro, meso and thermophiles. What exact temperature ranges these are etc.
+
+## Manually curated database
+Should have an accession, thermal range and an exact optimal temperature
---
+++ agents.py
@@ -0,0 +1,11 @@
+# imports
+from langchain_ollama import ChatOllama
+from langgraph.graph import StateGraph, START, END
+
+
+# Set model to be used
+OLLAMA_MODEL = "llama3.1"
+OLLAMA_BASE_URL = "http://localhost:11434"
+
+from langchain_ollama import ChatOllama
+from langgraph.graph import StateGraph, START, END
